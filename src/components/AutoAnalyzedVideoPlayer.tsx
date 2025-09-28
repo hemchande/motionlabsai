992fdc07aef41aaab0e4a94365e8059aad759ce9
hemchande <hemchandeisha@gmail.com>
2025-09-28 11:16:10 -0500

    Fix JSX syntax error and add simple video element
    - Add simple video element with id='video' and id='videoSource' (copied from enable_download_test.html)
    - Fix JSX parsing error by adding proper video element structure
    - Keep existing HTML-style controls that were already added
    - Now has both simple video element and working control functions

--- src/components/AutoAnalyzedVideoPlayer.tsx
+++ src/components/AutoAnalyzedVideoPlayer.tsx
@@ -1929,6 +1929,21 @@ export default function AutoAnalyzedVideoPlayer({
             {/* Video Player - Left Side */}
             <div className="lg:col-span-3">
               <div className="relative bg-black rounded-lg overflow-hidden flex items-center justify-center" style={getVideoContainerStyle()}>
+              {/* Simple video element (copied from enable_download_test.html) */}
+              <div className="video-container w-full h-full">
+                <video 
+                  id="video" 
+                  controls 
+                  width="100%" 
+                  height="100%" 
+                  style={{ background: 'black' }} 
+                  crossOrigin="anonymous" 
+                  preload="metadata"
+                >
+                  <source src="" type="video/mp4" id="videoSource" />
+                  Your browser does not support the video tag.
+                </video>
+              </div>
               {error ? (
                 <div className="text-center text-white p-6">
                   <AlertTriangle className="h-12 w-12 mx-auto mb-4 text-yellow-500" />
@@ -2254,7 +2269,7 @@ export default function AutoAnalyzedVideoPlayer({
                 >
                   Open in New Tab
                 </button>
-              </div>
+                </div>
 
               {/* Frame-by-Frame Controls - Moved to very bottom */}
               <div className="absolute bottom-8 left-4 right-4 flex items-center justify-center space-x-2">
